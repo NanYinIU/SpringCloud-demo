29a47c0129518d3254c22fd8379e34ab21a47d00
高国兴 <nanyiniu76@gmail.com>
2020-06-22 23:06:20 +0800

    添加nacos作为服务发现注册中心(配置部分)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SpringCloud-demo
-学习SpringCloud全家桶项目
+
+完成maven结构设置，将使用nacos作为服务发现注册中心和配置中心。#2020-06-22
--- provider/src/main/java/com/nanyin/discoveryserver/DiscoveryServerApplication.java
+++ provider/src/main/java/com/nanyin/discoveryserver/DiscoveryServerApplication.java
@@ -2,8 +2,11 @@ package com.nanyin.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+// 使用 EnableDiscoveryClient 注解标注服务注册发现功能
+@EnableDiscoveryClient
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
